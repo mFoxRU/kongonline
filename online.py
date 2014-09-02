3ff818f63e192f2bba5e647d6e193cfb079a471a
mFoxRU <mfoxru@gmail.com>
2014-09-03 01:59:32 +0400

    Added: Saving to SQLite database

--- online.py
+++ online.py
@@ -1,14 +1,19 @@
 __author__ = 'mFoxRU'
 
-from time import sleep
+from time import sleep, time
 
 from checker import check
+from saver import write_data
+
+db_file = 'kongonline.sqlite'
 
 
 def main():
     while 1:
-        print check()
-        sleep(100)
+        online, games = check()
+        timestamp = int(time())
+        write_data(timestamp, online, games, db_file)
+        sleep(5)
 
 if __name__ == '__main__':
     main()
